4fead3871e50d0035866f19d3e8bbd17b3c7b440
Justin Campbell <justin@justincampbell.me>
2012-04-05 09:21:46 -0400

    Add installer script

---
+++ install.sh
@@ -0,0 +1,6 @@
+cd ~
+ln -s .dotfiles/.gemrc        ~/.gemrc
+ln -s .dotfiles/.gvimrc.after ~/.gvimrc.after
+ln -s .dotfiles/.irbrc        ~/.irbrc
+ln -s .dotfiles/.janus/       ~/.janus
+ln -s .dotfiles/.profile      ~/.profile
