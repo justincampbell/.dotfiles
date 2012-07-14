9ccd443323e7023aecdf0e58a439b20812658afe
Justin Campbell <justin@justincampbell.me>
2012-07-14 15:11:57 -0400

    Add tmux configuration

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s .dotfiles/.gvimrc.after ~/.gvimrc.after
 ln -s .dotfiles/.irbrc        ~/.irbrc
 ln -s .dotfiles/.janus/       ~/.janus
 ln -s .dotfiles/.profile      ~/.profile
+ln -s .dotfiles/.tmux.conf    ~/.tmux.conf
