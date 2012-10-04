efb238e0698a727112a0a70e340740a03d7b6d2f
Justin Campbell <justin@justincampbell.me>
2012-10-04 16:33:39 -0400

    Rename .vimrc.after to .vimrc

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ ln -s .dotfiles/.irbrc ~/.irbrc
 ln -s .dotfiles/.janus/ ~/.janus
 ln -s .dotfiles/.profile ~/.profile
 ln -s .dotfiles/.tmux.conf ~/.tmux.conf
-ln -s .dotfiles/.vimrc.after ~/.vimrc.after
+ln -s .dotfiles/.vimrc ~/.vimrc
