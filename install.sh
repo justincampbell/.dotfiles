776a96605ec555c35f9359acdc601dc6b010f64f
Justin Campbell <justin@justincampbell.me>
2012-10-04 15:36:58 -0400

    Sort installer lines

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd ~
 ln -s .dotfiles/.gemrc        ~/.gemrc
-ln -s .dotfiles/.vimrc.after  ~/.vimrc.after
 ln -s .dotfiles/.irbrc        ~/.irbrc
 ln -s .dotfiles/.janus/       ~/.janus
 ln -s .dotfiles/.profile      ~/.profile
 ln -s .dotfiles/.tmux.conf    ~/.tmux.conf
+ln -s .dotfiles/.vimrc.after  ~/.vimrc.after
