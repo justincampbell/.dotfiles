966f8674d7ff6b1296dae0f20d3065d57d3a6d6a
Justin Campbell <justin@justincampbell.me>
2012-10-04 15:39:26 -0400

    Add git commit template

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd ~
 ln -s .dotfiles/.gemrc ~/.gemrc
+ln -s .dotfiles/.git_commit_template ~/.git_commit_template
 ln -s .dotfiles/.irbrc ~/.irbrc
 ln -s .dotfiles/.janus/ ~/.janus
 ln -s .dotfiles/.profile ~/.profile
