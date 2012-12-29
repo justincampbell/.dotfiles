e352a02a07058d9a03b94b9c6288e97ac85bd8f4
Justin Campbell <justin@justincampbell.me>
2012-12-28 19:25:38 -0500

    Remove remnants of Janus

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ ln -s .dotfiles/.ackrc ~/.ackrc
 ln -s .dotfiles/.gemrc ~/.gemrc
 ln -s .dotfiles/.git_commit_template ~/.git_commit_template
 ln -s .dotfiles/.irbrc ~/.irbrc
-ln -s .dotfiles/.janus/ ~/.janus
 ln -s .dotfiles/.profile ~/.profile
 ln -s .dotfiles/.tmux.conf ~/.tmux.conf
 ln -s .dotfiles/.vimrc ~/.vimrc
