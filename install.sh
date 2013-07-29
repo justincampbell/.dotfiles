6ca979a690fe70a30cbb9d0927228d05d29cbb47
Justin Campbell <justin@justincampbell.me>
2013-07-29 16:33:10 -0400

    Add slate config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s .dotfiles/.gemrc ~/.gemrc
 ln -s .dotfiles/.git_commit_template ~/.git_commit_template
 ln -s .dotfiles/.irbrc ~/.irbrc
 ln -s .dotfiles/.profile ~/.profile
+ln -s .dotfiles/.slate ~/.slate
 ln -s .dotfiles/.tmux.conf ~/.tmux.conf
 ln -s .dotfiles/.vimrc ~/.vimrc
 
