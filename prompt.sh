e1976b2582e1cc7d2d9db4e558cbaa4d558c7da2
Justin Campbell <justin@justincampbell.me>
2013-10-04 07:04:34 -0400

    Replace git-prompt with simpler script
    * Use git status --short
    * Source git-completion from homebrew package install
    * 🌈

---
+++ prompt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+orange=`tput setaf 166`
+cyan=`tput setaf 6`
+dark_gray=`tput setaf 236`
+reset=`tput sgr0`
+
+random_color() {
+  echo -n `jot -r 1 1 231 | xargs tput setaf`
+}
+
+prompt_command() {
+  set_ps1
+
+  [[ -d .git ]] &&
+  [[ `history 1` != *'git status'* ]] &&
+  echo -n ${dark_gray} &&
+  git status --branch --short --untracked=normal
+}
+
+set_ps1() {
+  PS1='\[${cyan}\]\W\[${orange}\]\[$(random_color)\]$ \[${reset}\]'
+}
+
+set_ps1
+PS2='\[${orange}\]> \[${reset}\]'
+
+PROMPT_COMMAND='prompt_command'
