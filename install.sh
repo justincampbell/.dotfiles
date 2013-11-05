7cfd26edcd66487a9d0aa1070d8cf10ff3cd6896
Justin Campbell <justin@justincampbell.me>
2013-11-05 14:25:54 -0500

    Symlink paths explicitly

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
-cd ~
-
-ln -s .dotfiles/.agignore ~/.agignore
-ln -s .dotfiles/.gemrc ~/.gemrc
-ln -s .dotfiles/.git_commit_template ~/.git_commit_template
-ln -s .dotfiles/.hushlogin ~/.hushlogin
-ln -s .dotfiles/.irbrc ~/.irbrc
-ln -s .dotfiles/.profile ~/.profile
-ln -s .dotfiles/.slate ~/.slate
-ln -s .dotfiles/.tmux.conf ~/.tmux.conf
-ln -s .dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.agignore ~/.agignore
+ln -s ~/.dotfiles/.gemrc ~/.gemrc
+ln -s ~/.dotfiles/.git_commit_template ~/.git_commit_template
+ln -s ~/.dotfiles/.hushlogin ~/.hushlogin
+ln -s ~/.dotfiles/.irbrc ~/.irbrc
+ln -s ~/.dotfiles/.profile ~/.profile
+ln -s ~/.dotfiles/.slate ~/.slate
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 git config --global color.ui true
 git config --global commit.template ~/.git_commit_template
@@ -17,4 +15,3 @@ git config --global push.default simple
 git config --global user.email "justin@justincampbell.me"
 git config --global user.name "Justin Campbell"
 git config --global web.browser open
-
