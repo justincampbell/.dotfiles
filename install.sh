c35ff67817768fd25c26873d0d00325eb0c48bc7
Justin Campbell <justin@justincampbell.me>
2013-11-19 08:43:08 -0500

    Silence symlink already existing warnings

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
-ln -s ~/.dotfiles/.agignore ~/.agignore
-ln -s ~/.dotfiles/.gemrc ~/.gemrc
-ln -s ~/.dotfiles/.git_commit_template ~/.git_commit_template
-ln -s ~/.dotfiles/.hushlogin ~/.hushlogin
-ln -s ~/.dotfiles/.irbrc ~/.irbrc
-ln -s ~/.dotfiles/.profile ~/.profile
-ln -s ~/.dotfiles/.rdebugrc ~/.rdebugrc
-ln -s ~/.dotfiles/.slate ~/.slate
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+#!/bin/bash -ex
+
+ln -fs ~/.dotfiles/.agignore ~/.agignore
+ln -fs ~/.dotfiles/.gemrc ~/.gemrc
+ln -fs ~/.dotfiles/.git_commit_template ~/.git_commit_template
+ln -fs ~/.dotfiles/.hushlogin ~/.hushlogin
+ln -fs ~/.dotfiles/.irbrc ~/.irbrc
+ln -fs ~/.dotfiles/.profile ~/.profile
+ln -fs ~/.dotfiles/.rdebugrc ~/.rdebugrc
+ln -fs ~/.dotfiles/.slate ~/.slate
+ln -fs ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -fs ~/.dotfiles/.vimrc ~/.vimrc
 
 git config --global color.ui true
 git config --global commit.template ~/.git_commit_template
