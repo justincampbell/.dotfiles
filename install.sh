ec2cf71c3f821ae7caecbb1f4cc90d7fed5bcf1b
Justin Campbell <justin@justincampbell.me>
2015-01-06 14:11:16 -0500

    Git push current branch

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ git config --global commit.template ~/.git_commit_template
 git config --global github.user "justincampbell"
 git config --global help.autocorrect 25
 git config --global pull.ff only
-git config --global push.default simple
+git config --global push.default current
 git config --global user.email "justin@justincampbell.me"
 git config --global user.name "Justin Campbell"
 git config --global web.browser open
