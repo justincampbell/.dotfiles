2a1fccac3ea8ae7ee175383be32f40163e03ba98
Justin Campbell <justin@justincampbell.me>
2022-09-08 16:40:43 -0400

    Codespaces: Fixes for Debian 11

--- install-codespaces.sh
+++ install-codespaces.sh
@@ -1,30 +1,33 @@
 #!/bin/bash -ex
 
-sudo add-apt-repository ppa:neovim-ppa/stable --no-update -y
-sudo apt update
-sudo apt upgrade -y
-
 # Packages
 sudo apt -o DPkg::Lock::Timeout=600 install \
   athena-jot \
   bat \
   fd-find \
-  neovim \
   python-dev \
   python3-dev \
   python3-pip \
+  software-properties-common \
   tmux \
   tree \
   -y
 
 sudo ln -s $(which fdfind) /usr/bin/fd
+if ! [[ -x /usr/bin/bat ]]; then
+  sudo ln -s $(which batcat) /usr/bin/bat
+fi
 
 # Bat Theme
-curl -fLo "$(batcat --config-dir)/themes/railscasts/railscasts.tmTheme" --create-dirs \
+curl -fLo "$(bat --config-dir)/themes/railscasts/railscasts.tmTheme" --create-dirs \
   https://raw.githubusercontent.com/tdm00/sublime-theme-railscasts/master/RailsCastsColorScheme.tmTheme
-batcat cache --build
+bat cache --build
 
 # Neovim
+cd $(mktemp -d) && \
+  curl -fLo nvim-linux64.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb && \
+  sudo apt install ./nvim-linux64.deb -y
+sudo apt install python3-neovim -y
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim --headless +PlugInstall +qa
